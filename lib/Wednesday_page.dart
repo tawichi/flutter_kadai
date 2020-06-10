1194c45851cb39b5f42aa2e463e7085c252555a0
tawichi <otai0811@gmail.com>
2020-06-11 06:11:57 +0900

    ラジオボタン 押せるようになった

--- lib/Wednesday_page.dart
+++ lib/Wednesday_page.dart
@@ -5,62 +5,100 @@ class WednesdayPage extends StatelessWidget {
   String _message;
   bool _checked = false;
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold (
-      appBar: AppBar (
-        title: Text ( '水曜締め切り' ) ,
-      ) ,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text(
-              '計算機基礎   ',
-          style: TextStyle(fontSize: 16.0),
-          ),
-
-          Row(
-
-            children: <Widget>[
-
-              Checkbox(
-                value:  _checked,
-                onChanged: checkChanged,
-              ),
-              Text('提出済み',)
-
-            ],
-          ),
-          Text('コン実',
-            style: TextStyle(fontSize: 16.0),),
-          Row(
-            children: <Widget>[
-
-              Checkbox(
-                value:  _checked,
-                onChanged: checkChanged,
-              ),
-              Text('提出済み'),
+    return   new MyHomePage(
 
 
-            ],
 
+      );
 
-          ),
-          Text('備考欄'),
-          TextField(),
-        ],
-
-      ) ,
-    );
-  }
-  void checkChanged (bool value){
-    setState((){
-      _checked = value;
-      _message = value ? 'checked' : 'not checked';
-    });
   }
 }
 
-void setState(Null Function() param0) {
+class MyHomePage extends StatefulWidget{
+  MyHomePage({this.title}): super();
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+
+//インスタンスからウィジェットを作る
+
+class _MyHomePageState extends State<MyHomePage>{
+  String _message;
+  bool _checked1 = false;
+  bool _checked2 = false;
+
+
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('水曜締め切り'),
+      ),
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '計算機基礎   ',
+              style: TextStyle(fontSize: 16.0),
+            ),
+
+            Row(
+
+              children: <Widget>[
+
+                Checkbox(
+                  value:  _checked1,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checked1 = value;
+                    });
+                  },
+                ),
+                Text('提出済み',)
+
+              ],
+            ),
+            Text('コン実',
+              style: TextStyle(fontSize: 16.0),),
+            Row(
+              children: <Widget>[
+
+                Checkbox(
+                  value:  _checked2,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _checked2 = value;
+                    });
+                  },
+                ),
+                Text('提出済み'),
+
+
+              ],
+
+
+            ),
+            Text('備考欄'),
+            TextField(),
+          ],
+
+        ) ,
+      );
+
+
+
+
+  }
+
+
+
 }
